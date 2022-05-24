b2694671d5fed4984e84cc0455e96f11dc644b31
ubaumann <github@m.ubaumann.ch>
2022-05-25 00:21:37 +0200

    Fix deployment

--- netaut_cicd_task/main.py
+++ netaut_cicd_task/main.py
@@ -3,7 +3,7 @@ from rich.console import Console
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 
-from netaut_cicd_task.nr_tasks import get_vrf_ospf_bgp, desired_rpc
+from netaut_cicd_task.nr_tasks import get_vrf_ospf_bgp, desired_rpc, edit_config
 
 app = typer.Typer()
 console = Console()
@@ -49,7 +49,12 @@ def get_config(ctx: typer.Context) -> None:
 def validate(ctx: typer.Context) -> None:
     """Load configuration into candidate store and exit with validate"""
     nr = ctx.obj
-    console.print(f"validated")
+    result = nr.run(task=edit_config, nr=nr, candidate=True)
+    print_result(result)
+    if result.failed:
+        console.print("Validation failed")
+        raise typer.Exit(code=1)
+    console.print("Validation successful")
     raise typer.Exit(code=0)
 
 
@@ -57,9 +62,14 @@ def validate(ctx: typer.Context) -> None:
 def deploy(ctx: typer.Context) -> None:
     """Deploy configuration into running store"""
     nr = ctx.obj
-    print_result(nr.run(task=desired_rpc, nr=nr))
-    console.print(f"Deploy failed")
-    raise typer.Exit(code=1)
+    result = nr.run(task=edit_config, nr=nr, candidate=False)
+    print_result(result)
+    print(f"Failed hosts: {result.failed_hosts}")
+    if result.failed:
+        console.print("Deployment failed")
+        raise typer.Exit(code=1)
+    console.print("Deployment successful")
+    raise typer.Exit(code=0)
 
 
 @app.command()
--- netaut_cicd_task/nr_tasks.py
+++ netaut_cicd_task/nr_tasks.py
@@ -1,8 +1,12 @@
-from typing import Union
 from nornir.core import Nornir
-from nornir.core.task import Result, MultiResult, Task
+from nornir.core.task import Task, Result
 from nornir.core.filter import F
-from nornir_netconf.plugins.tasks import netconf_get_config
+from nornir_netconf.plugins.tasks import (
+    netconf_get_config,
+    netconf_edit_config,
+    netconf_commit,
+    netconf_lock,
+)
 from nornir_jinja2.plugins.tasks import template_file
 
 from netaut_cicd_task.filter import split_interface, first_host, networkmask
@@ -12,6 +16,34 @@ finja_filter = {
     "first_host": first_host,
     "networkmask": networkmask,
 }
+
+
+def discard_config(
+    task: Task,
+) -> Result:
+    manager = task.host.get_connection("netconf", task.nornir.config)
+    rpc = manager.discard_changes()
+    result = {
+        "error": rpc.error,
+        "errors": rpc.errors,
+        "ok": rpc.ok,
+        "rpc": rpc,
+    }
+    return Result(host=task.host, result=result, failed=not rpc.ok)
+
+
+def netconf_validate(
+    task: Task,
+) -> Result:
+    manager = task.host.get_connection("netconf", task.nornir.config)
+    rpc = manager.validate()
+    result = {
+        "error": rpc.error,
+        "errors": rpc.errors,
+        "ok": rpc.ok,
+        "rpc": rpc,
+    }
+    return Result(host=task.host, result=result, failed=not rpc.ok)
 
 
 def get_vrf_ospf_bgp(task: Task) -> None:
@@ -37,10 +69,10 @@ def get_vrf_ospf_bgp(task: Task) -> None:
     )
 
 
-def desired_rpc(task: Task, nr: Nornir) -> Union[Result, MultiResult]:
+def desired_rpc(task: Task, nr: Nornir) -> None:
     rr_hosts = nr.filter(F(tags__contains="RR")).inventory.hosts  # type: ignore
     edge_hosts = nr.filter(F(tags__contains="edge")).inventory.hosts  # type: ignore
-    native = task.run(
+    task.run(
         template_file,
         template="native.j2",
         path="templates",
@@ -48,4 +80,28 @@ def desired_rpc(task: Task, nr: Nornir) -> Union[Result, MultiResult]:
         rr_hosts=rr_hosts,
         edge_hosts=edge_hosts,
     )
-    return native
+
+
+def edit_config(task: Task, nr: Nornir, candidate: bool = True) -> None:
+    desired_result = task.run(desired_rpc, nr=nr)
+    # task.run(netconf_lock, datastore="candidate", operation="lock")
+    task.run(discard_config)
+    task.run(
+        netconf_edit_config,
+        config=desired_result[1].result,
+        target="candidate",
+    )
+    subtree_filter = (
+        """<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native"></native>"""
+    )
+    task.run(
+        netconf_get_config,
+        source="candidate",
+        path=subtree_filter,
+        filter_type="subtree",
+    )
+    # task.run(netconf_lock, datastore="candidate", operation="unlock")
+    if not candidate:
+        task.run(netconf_commit)
+    else:
+        task.run(netconf_validate)
